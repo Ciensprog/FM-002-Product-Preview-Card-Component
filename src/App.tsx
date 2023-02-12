6bdc9fc1ed8d348b798f6023f29bb0e953029282
Ciensprog <jorg.leonrd@gmail.com>
2023-02-12 17:16:09 -0600

    Add action button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { IconCart } from './assets/icons/cart'
+
 import { Attribution } from './components/Attribution'
 
 /*
@@ -11,11 +13,15 @@ export function App() {
     <>
       <main className="my-10 z-10">
         <div className="bg-white grid grid-cols-2 max-w-[37.5rem] overflow-hidden rounded-[0.625rem] w-full">
-          <section className="h-full w-full">
-            <figure>
-              <img src="product.jpg" height={450} width={300} alt="Product" />
-            </figure>
-          </section>
+          <figure className="h-full w-full">
+            <img
+              src="product.jpg"
+              className="h-full object-cover w-full"
+              height={450}
+              width={300}
+              alt="Product"
+            />
+          </figure>
           <section className="p-8">
             <p className="font-medium text-aurometal-400 text-xs tracking-[0.3125rem] uppercase">
               Perfume
@@ -27,7 +33,7 @@ export function App() {
               A floral, solar and voluptuous interpretation composed by Olivier
               Polge, Perfumer-Creator for the House of CHANEL.
             </p>
-            <div className="flex gap-7 items-center mt-7">
+            <div className="flex gap-7 items-center mb-[1.875rem] mt-7">
               <div className="font-bold font-fraunces leading-8 text-[2rem] text-aquamarine-400">
                 $149.99
               </div>
@@ -35,6 +41,10 @@ export function App() {
                 $169.99
               </s>
             </div>
+            <button className="bg-aquamarine-400 flex font-bold gap-3 items-center justify-center leading-4 py-4 rounded-lg text-sm text-white transition-colors w-full hover:bg-aquamarine-700">
+              <IconCart />
+              <span className="">Add to Cart</span>
+            </button>
           </section>
         </div>
       </main>
