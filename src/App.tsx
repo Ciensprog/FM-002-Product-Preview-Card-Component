69afd7a50073d9d9b01ed2188c9127c28a03dc58
Ciensprog <jorg.leonrd@gmail.com>
2023-02-12 16:20:27 -0600

    Add product image

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,17 @@ export function App() {
   return (
     <>
       <main className="my-10 z-10">
-        <div className="bg-white p-5 rounded-[0.625rem]">
-          <h1 className="font-fraunces font-bold leading-8 text-[2rem]">
-            Gabrielle Essence Eau De Parfum
-          </h1>
+        <div className="bg-white grid grid-cols-2 max-w-[37.5rem] overflow-hidden rounded-[0.625rem] w-full">
+          <section className="h-full w-full">
+            <figure>
+              <img src="product.jpg" height={450} width={300} alt="Product" />
+            </figure>
+          </section>
+          <section className="p-8">
+            <h1 className="font-fraunces font-bold leading-8 text-[2rem]">
+              Gabrielle Essence Eau De Parfum
+            </h1>
+          </section>
         </div>
       </main>
 
